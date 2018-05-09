b8e58cc226176215d3e7570c0f9a59ab027b086d
jvillalv <julio.villalva@autozone.com>
2018-05-08 18:29:12 -0600

    adding webpack and exports tutorial

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+// entry
+// outpun bundle 
+
+const path = require('path');
+
+module.exports ={
+    entry :"./src/app.js",
+    output: {
+        path:path.join(__dirname,'public'),
+        filename:'bundle.js'
+    },
+    mode:"development"
+};
